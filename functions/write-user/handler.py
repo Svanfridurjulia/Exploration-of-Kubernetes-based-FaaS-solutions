1afef95ed9bbab15d1fc8e7f464419a8628c382b
Svanfridurjulia <svanfridur20@ru.is>
2023-04-28 15:23:34 +0000

    Updating write-user code and setting correct image version in helm chart

--- functions/write-user/handler.py
+++ functions/write-user/handler.py
@@ -4,36 +4,40 @@ import json
 import hashlib
 
 
-# Get the AWS credentials from environment variables
-# with open("/var/openfaas/secrets/AWS_ACCESS_KEY_ID", "r") as f:
-#     aws_access_key_id = f.read().strip()
+# # Get the AWS credentials from environment variables
+# # with open("/var/openfaas/secrets/AWS_ACCESS_KEY_ID", "r") as f:
+# #     aws_access_key_id = f.read().strip()
 
-# with open("/var/openfaas/secrets/AWS_SECRET_ACCESS_KEY", "r") as f:
-#     aws_secret_access_key = f.read().strip()
+# # with open("/var/openfaas/secrets/AWS_SECRET_ACCESS_KEY", "r") as f:
+# #     aws_secret_access_key = f.read().strip()
 
-aws_access_key_id = os.environ["openfaas_secret_aws-credentials_aws_access_key"]
-aws_secret_access_key = os.environ["openfaas_secret_aws-credentials_aws_secret_key"]
+# aws_access_key_id = os.environ["openfaas_secret_aws-credentials_aws_access_key"]
+# aws_secret_access_key = os.environ["openfaas_secret_aws-credentials_aws_secret_key"]
 
 
-# aws_access_key_id = os.environ["AWS_ACCESS_KEY_ID"]
-# aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
-aws_default_region = 'eu-west-1'
+# # aws_access_key_id = os.environ["AWS_ACCESS_KEY_ID"]
+# # aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
+# aws_default_region = 'eu-west-1'
 
-# Create a boto3 session with the provided credentials
-session = boto3.Session(
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key,
-    region_name=aws_default_region,
-)
+# # Create a boto3 session with the provided credentials
+# session = boto3.Session(
+#     aws_access_key_id=aws_access_key_id,
+#     aws_secret_access_key=aws_secret_access_key,
+#     region_name=aws_default_region,
+# )
 
-# Initialize a DynamoDB client using the session
-dynamodb = session.resource("dynamodb")
+# # Initialize a DynamoDB client using the session
+# dynamodb = session.resource("dynamodb")
 
 
 def handle(req):
-    # Parse the input JSON string
+
     req_data = json.loads(req)
 
+    # Create the DynamoDB resource
+    dynamodb = boto3.resource('dynamodb')
+
+    # Get the DynamoDB table
     table = dynamodb.Table('users')
 
     # Perform a scan operation to retrieve all items
