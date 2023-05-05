4c2b16745128b18c1debe0354cc425cb2428c640
Ingunn-B <ingunn20@ru.is>
2023-05-05 09:29:28 +0000

    Userbutton and userprofile tests

--- React_frontend/web_app/src/components/UserButton/UserButton.test.js
+++ React_frontend/web_app/src/components/UserButton/UserButton.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import { UserButton } from './UserButton';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
 
 describe('UserButton', () => {
     test('should render the user button', () => {
@@ -16,10 +16,10 @@ describe('UserButton', () => {
         expect(dropdown).toBeNull();
     });
 
-    test('should show the dropdown when the user button is clicked', () => {
+    test('dropdown displayed when the profile button is clicked', () => {
         render(
             <Router basename="">
-                <UserButton option1="Profile" link1="/profile" option2="Log out" link2="/logout" />
+                <UserButton option1="Profile" link1="/profile" option2="Log out" link2="/" />
             </Router>
         );
         const { getByTestId } = render(<UserButton />);
@@ -29,59 +29,45 @@ describe('UserButton', () => {
         expect(dropdown).toBeInTheDocument();
     });
 
-    //   test('should hide the dropdown when the user button is clicked again', () => {
-    //     render(
-    //         <Router basename="">
-    //           <UserButton option1="Profile" link1="/profile" option2="Log out" link2="/logout" />
-    //         </Router>
-    //       );
-    //     const { getByTestId, queryByTestId } = render(<UserButton />);
-    //     const userButton = queryByTestId('profileButton');
-    //     fireEvent.click(userButton);
-    //     let dropdown = getByTestId('dropdownDiv');
-    //     expect(dropdown).toBeInTheDocument();
-    //     fireEvent.click(userButton);
-    //     dropdown = queryByTestId('dropdownDiv');
-    //     expect(dropdown).toBeNull();
-    //   });
+    test('dropdown hidden when the profile button is clicked again', () => {
+        render(
+            <Router basename="">
+                <UserButton option1="Profile" link1="/profile" option2="Log out" link2="/" />
+            </Router>
+        );
+        const { getByTestId, queryByTestId } = render(<UserButton />);
+        const userButton = getByTestId('Profile').querySelector('[data-testid="profileButton"]')
+        fireEvent.click(userButton);
+        let dropdown = getByTestId('dropdownDiv');
+        expect(dropdown).toBeInTheDocument();
+        fireEvent.click(userButton);
+        dropdown = queryByTestId('dropdownDiv');
+        expect(dropdown).toBeNull();
+    });
 
-    //   test('should show the first option in the dropdown', () => {
-    //     const { getByTestId } = render(
-    //       <UserButton option1="Option 1" link1="/" option2="Option 2" link2="/dashboard" />
-    //     );
-    //     const userButton = getByTestId('userButton');
-    //     fireEvent.click(userButton);
-    //     const dropButton1 = getByTestId('dropButton1');
-    //     expect(dropButton1).toHaveTextContent('Option 1');
-    //   });
+    test('Dropdown shows option1', () => {
+        render(
+            <Router basename="">
+                <UserButton option1="Dashboard" link1="/dashboard" option2="Log out" link2="/" />
+            </Router>
+        );
+        const { getByTestId } = render(<UserButton />);
+        const userButton = getByTestId('Dashboard').querySelector('[data-testid="profileButton"]')
+        fireEvent.click(userButton);
+        const dropButton1 = getByTestId('dropButton1');
+        expect(dropButton1).toHaveTextContent('Dashboard');
+    });
 
-    //   test('should show the second option in the dropdown', () => {
-    //     const { getByTestId } = render(
-    //       <UserButton option1="Option 1" link1="/" option2="Option 2" link2="/dashboard" />
-    //     );
-    //     const userButton = getByTestId('userButton');
-    //     fireEvent.click(userButton);
-    //     const dropButton2 = getByTestId('dropButton2');
-    //     expect(dropButton2).toHaveTextContent('Option 2');
-    //   });
-
-    //   test('should link to the correct URL for the first option', () => {
-    //     const { getByTestId } = render(
-    //       <UserButton option1="Option 1" link1="/" option2="Option 2" link2="/dashboard" />
-    //     );
-    //     const userButton = getByTestId('userButton');
-    //     fireEvent.click(userButton);
-    //     const dropButton1 = getByTestId('dropButton1');
-    //     expect(dropButton1).toHaveAttribute('href', '/');
-    //   });
-
-    //   test('should link to the correct URL for the second option', () => {
-    //     const { getByTestId } = render(
-    //       <UserButton option1="Option 1" link1="/" option2="Option 2" link2="/dashboard" />
-    //     );
-    //     const userButton = getByTestId('userButton');
-    //     fireEvent.click(userButton);
-    //     const dropButton2 = getByTestId('dropButton2');
-    //     expect(dropButton2).toHaveAttribute('href', '/dashboard');
-    //   });
+    test('Dropdown shows option2', () => {
+        render(
+            <Router basename="">
+                <UserButton option1="Dashboard" link1="/dashboard" option2="Log out" link2="/" />
+            </Router>
+        );
+        const { getByTestId } = render(<UserButton />);
+        const userButton = getByTestId('Dashboard').querySelector('[data-testid="profileButton"]')
+        fireEvent.click(userButton);
+        const dropButton2 = getByTestId('dropButton2');
+        expect(dropButton2).toHaveTextContent('Log out');
+    });
 });
