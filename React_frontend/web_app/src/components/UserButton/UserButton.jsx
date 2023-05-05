f114ef112173555935afba04e03348b9d0ea51e2
Ingunn-B <ingunn20@ru.is>
2023-05-05 13:26:37 +0000

    userbutton comments

--- React_frontend/web_app/src/components/UserButton/UserButton.jsx
+++ React_frontend/web_app/src/components/UserButton/UserButton.jsx
@@ -5,9 +5,12 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 import "./styles.css";
 
 export const UserButton = ({option1, link1, option2, link2}) => {
+    // Declare state variables for managing the dropdown
     const [dropdown, setDropdown] = useState(false);    
 
+    // Event handler for the dropdown button click
     const handleDropdownClick = () => {
+        // Toggle the dropdown state
         setDropdown(!dropdown);
     }
 
@@ -16,6 +19,7 @@ export const UserButton = ({option1, link1, option2, link2}) => {
             <button data-testid="profileButton" className="userButton" onClick={handleDropdownClick} >
                 <FontAwesomeIcon icon={faUser} />
             </button>
+            {/* Render the dropdown menu if the dropdown state is true */}
             {(dropdown) ? (
                     <div data-testid="dropdownDiv" className="dropdown">
                         <button data-testid="dropButton1" className="dropButton"><Link className="linked" to={link1}>{option1}</Link></button>
@@ -23,6 +27,7 @@ export const UserButton = ({option1, link1, option2, link2}) => {
                     </div>
                 )
                 :(
+                    // Render nothing if the dropdown state is false
                     null
                 )
             }
