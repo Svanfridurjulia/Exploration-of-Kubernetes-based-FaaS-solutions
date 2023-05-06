3cad1dc2e485d709824ef1d9e305971d1e19a321
Svanfridurjulia <svanfridur20@ru.is>
2023-05-06 16:00:06 +0000

    updating cors headers in get-translation function

--- functions/get-translation/handler.py
+++ functions/get-translation/handler.py
@@ -7,6 +7,22 @@ def handle(req):
 
     Parameter req: the english text to be translated to Spanish
     """
+
+    req_json = json.loads(req)
+    print(req_json)
+    method = req_json.get('method', '')
+    print(method)
+    # Set the CORS headers
+    headers = {
+        'Access-Control-Allow-Origin': 'http://web-app.fabulousasaservice.com',
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Content-Type': 'application/json'
+    }
+
+    if method == 'OPTIONS':
+        return json.dumps((204, '', headers))
+
     params = {'q': req, 'langpair': 'en|es'}
     # Make GET request to the MyMemory API with the text to be translated and language pair
     response = requests.get('https://api.mymemory.translated.net/', params=params)
@@ -15,13 +31,8 @@ def handle(req):
     # Extract the parsed translated text
     translatedText = data['responseData']['translatedText']
     
-    # Set the CORS headers
-    headers = {
-        'Access-Control-Allow-Origin': 'http://web-app.fabulousasaservice.com',
-        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        'Content-Type': 'application/json'
-    }
+
+    print(req)
     
     # Return the translated text along with the CORS headers
-    return (200, translatedText, headers)
+    return json.dumps(200, translatedText, headers)
