bf1f8a401da51454f17d1e4e03564ede3d939294
Svanfridurjulia <svanfridur20@ru.is>
2023-05-14 19:36:58 +0000

    prepping for demo

--- react_frontend/web_app/src/services/FunctionServices.js
+++ react_frontend/web_app/src/services/FunctionServices.js
@@ -140,7 +140,7 @@ export function sendEmailGoFunction(username) {
 export async function demoPythonFunction() {
     try {
         // Send a POST request to the demo endpoint on the server.
-        const resp = await fetch(FUNCTIONS_URL + 'demo-function5', {
+        const resp = await fetch(FUNCTIONS_URL + 'demo-function6', {
             method: "POST",
             headers: {
                 'Content-type': 'application/json'
