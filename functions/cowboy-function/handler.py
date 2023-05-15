1266e3c365edca82e2c503b92aff0c25f2f6b0c5
Svanfridurjulia <svanfridur20@ru.is>
2023-05-15 09:24:30 +0000

    cowboy function

---
+++ functions/cowboy-function/handler.py
@@ -0,0 +1,22 @@
+def handle(event, context):
+     
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Content-Type': 'application/json'
+    }
+
+    #handle OPTIONS request
+
+    if event.method == 'OPTIONS':
+        return {
+            "statusCode": 204,
+            "body": "p",
+            "headers": headers
+        }
+    return {
+        "statusCode": 200,
+        "body": "Takk fyrir okkur!",
+        "headers": headers
+    }
